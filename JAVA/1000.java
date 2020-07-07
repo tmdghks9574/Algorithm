c9752e4d4f5da0aea56ece6fcdc2dc26e6a85e61
SeungHwan Kim <tmdghks9574@gmail.com>
2020-07-07 16:09:13 +0900

    algorithm

---
+++ JAVA/1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] argc) {
+        int a,b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a+b);
+    }
+}
